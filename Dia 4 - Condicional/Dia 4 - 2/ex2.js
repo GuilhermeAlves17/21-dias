b527246a1937659cc0a2b65b51b1707bede6fb65
Guilherme Alves <guilhermealves17004@gmail.com>
2023-09-03 20:40:01 -0300

    Atualização do código

--- Dia 4 - Condicional/Dia 4 - 2/ex2.js
+++ Dia 4 - Condicional/Dia 4 - 2/ex2.js
@@ -1,15 +1,29 @@
 // variáveis
-let nome = prompt("Digite seu nome: ")
-let idade = parseInt(prompt("Digite sua idade: "))
-let cnh = prompt ("Você possuí CNH? (s/n)")
-let car = prompt ("Você possuí carro? (s/n)")
+let nome 
+let idade 
+let cnh = false
+let car = false
+
+//Pedindo informações
+nome = prompt("Digite seu nome")
+idade = parseInt(prompt("Digite sua idade"))
+let temCNH = prompt("Você tem carteira de motorista? (s/n)")
+if (temCNH == "s"){
+    cnh = true
+}
+let temCar = prompt("Você tem carro? (s/n)")
+if (temCar == "s"){
+    car = true
+}
+
+
 
 //if e else 
- if (idade<18 || cnh == "n"){
+ if (idade < 18 || !cnh) {
     console.log(nome,", você não pode dirigir.")
- }else if (idade>=18 && cnh == "s" && car == "n"){
+ }else if (idade>=18 && cnh && !car){
     console.log(nome,", você pode dirigir mas não tem um carro.")
- }else if(idade>=18 && cnh == "s" && car == "s"){
+ }else if(idade>=18 && cnh && car){
     console.log(nome,", você será o motorista!")
  }else{
     console.log("Comando inválido, por favor reinicie a página.")
