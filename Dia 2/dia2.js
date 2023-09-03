823f6503d3eeee9a363188b317a6f9df35cc3ba6
Guilherme Augusto Alves <135280964+GuilhermeAlves17@users.noreply.github.com>
2023-09-03 20:42:00 -0300

    Delete Dia 2 directory

--- Dia 2/dia2.js
+++
@@ -1,8 +0,0 @@
-let tipotexto = "Aprendi a criar variáveis"
-console.log("Estou no dia 2, aprendi git")
-console.log(tipotexto)
-let number = 3
-let number2 = 4
-console.log("Número 1:", number)
-console.log("Número 2:", number2)
-console.log("Número 1 + Número 2 =", number+number2)
